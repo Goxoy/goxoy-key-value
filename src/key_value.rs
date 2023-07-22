16c0d45058dc0cc9261235d2fb43cac9f831ea05
Omer GOKSOY <goksoy.omer@gmail.com>
2023-07-23 00:06:18 +0300

    Update key_value.rs

--- src/key_value.rs
+++ src/key_value.rs
@@ -1,11 +1,10 @@
-use std::borrow::BorrowMut;
-
 use rusqlite::Connection;
 
 #[derive(Debug)]
 pub struct KeyValueDb {
+    pub db_open:bool,
     pub db_obj:Connection,
-    pub port_no:usize,
+    pub db_path:String,
 }
 
 impl KeyValueDb {
@@ -16,17 +15,20 @@ impl KeyValueDb {
             Ok(_) => false,
             Err(_) => true
         };
-
         let db_conn:Connection = Connection::open(full_path.clone()).unwrap();
         if create_table==true{
             let _db_result=db_conn.execute("CREATE TABLE kv_list ( key TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL )", ()).unwrap();
         }
         KeyValueDb{
+            db_open:true,
             db_obj:db_conn,
-            port_no:0,
+            db_path:full_path.clone(),
         }
     }
     pub fn add(&mut self,key:&str,value:&str) -> bool {
+        if self.db_open==false {
+            return false;
+        }
         let insert_result=self.db_obj .execute("INSERT INTO kv_list (key, value) VALUES (?1, ?2)",&[&key, &value]);
         if insert_result.is_ok(){
             if insert_result.unwrap()>0{
@@ -36,22 +38,24 @@ impl KeyValueDb {
         false
     }
     pub fn flush(&mut self) -> bool{
-        let db_obj=self.db_obj.borrow_mut();
-        let db_path=db_obj.path();
-        if db_path.is_some(){
-            let db_path=db_path.unwrap();
-            if std::fs::metadata(db_path).is_ok(){
-                let close_result=db_obj.close();
-                if close_result.is_ok(){
-                    if std::fs::remove_file(db_path).is_ok(){
-                        return true;
-                    }
-                }
+        self.close();
+        if std::fs::metadata(&self.db_path).is_ok(){
+            if std::fs::remove_file(&self.db_path).is_ok(){
+                return true;
             }
         }
         false
     }
+    pub fn close(&mut self){
+        if self.db_open==true {
+            self.db_obj=Connection::open_in_memory().unwrap();
+            self.db_open=false;
+        }
+    }
     pub fn delete(&mut self,key:&str) ->bool {
+        if self.db_open==false {
+            return false;
+        }
         let insert_result=self.db_obj .execute("DELETE FROM kv_list WHERE key = ?1",&[&key]);
         if insert_result.is_ok(){
             if insert_result.unwrap()>0{
@@ -61,6 +65,10 @@ impl KeyValueDb {
         false
     }
     pub fn get_value(&mut self,key:&str) -> Option<String> {
+        if self.db_open==false {
+            return None;
+        }
+
         let stmt = self.db_obj.prepare("SELECT value FROM kv_list WHERE key=:key");
         let mut result_str=String::new();
         let mut founded=false;
@@ -97,8 +105,8 @@ fn full_test() {
     //kv_obj.add("key3", "value");
     //kv_obj.add("key4", "value");
     //kv_obj.delete("key3");
-    let rst=kv_obj.get_value("key3");
-    dbg!(rst);
+    //let rst=kv_obj.get_value("key3");
+    //dbg!(rst);
     
     
     assert_eq!(true,true)
