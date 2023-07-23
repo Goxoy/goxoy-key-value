0d734c17ff0a5eb1ff9713ee12cfdb5423e080ea
Omer GOKSOY <42944329+omergoksoy@users.noreply.github.com>
2023-07-23 18:07:21 +0300

    Update key_value.rs

--- src/key_value.rs
+++ src/key_value.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
-
 use rusqlite::Connection;
 
 #[derive(Debug)]
 pub struct KeyValueDb {
     pub list:HashMap<String,String>,
     pub db_open:bool,
+    pub record_loaded:bool,
     pub db_obj:Connection,
     pub db_path:String,
 }
@@ -18,54 +18,89 @@ impl KeyValueDb {
             Ok(_) => false,
             Err(_) => true
         };
-        let mut tmp_list:HashMap<String,String>=HashMap::new();
         let db_conn:Connection = Connection::open(full_path.clone()).unwrap();
         if create_table==true{
             let _db_result=db_conn.execute("CREATE TABLE kv_list ( key TEXT NOT NULL PRIMARY KEY, value TEXT NOT NULL )", ()).unwrap();
-        }else{
-            let stmt = db_conn.prepare("SELECT value FROM kv_list WHERE key=:key");
+        }
+        KeyValueDb{
+            list:HashMap::new(),
+            db_open:true,
+            record_loaded:false,
+            db_obj:db_conn,
+            db_path:full_path.clone(),
+        }
+    }
+    pub fn load_records(&mut self){
+        if self.db_open==true && self.record_loaded==false {
+            let stmt = self.db_obj.prepare("SELECT * FROM kv_list");
             if stmt.is_ok(){
                 let mut stmt=stmt.unwrap();
                 stmt.query_map(
                     (), 
                     |row| {
-                        let row_result:String=row.get(0).unwrap();
-                        println!("row_result: {}",row_result);
-                        Ok(row_result)
+                        let key_result:String=row.get(0).unwrap();
+                        let value_result:String=row.get(1).unwrap();
+                        Ok((key_result,value_result))
                     }
                 ).into_iter().for_each(|person| {
                     person.for_each(|s|{
-                        //self.list.insert(k, v)
-                        //founded=true;
-                        let result_str=s.unwrap();
-                        println!("result_str: {}",result_str);
+                        let (key_result,value_result)=s.unwrap();
+                        self.list.insert(key_result, value_result);
                     });
                 });
             }
-        }
-        KeyValueDb{
-            list:tmp_list,
-            db_open:true,
-            db_obj:db_conn,
-            db_path:full_path.clone(),
+            self.record_loaded=true;
         }
     }
-    pub fn load_all(&mut self){
-        self.list.clear();
-       
-    }
-    pub fn add(&mut self,key:&str,value:&str) -> bool {
+    
+    pub fn set_value(&mut self,key:&str,value:&str) -> bool {
         if self.db_open==false {
             return false;
         }
-        let insert_result=self.db_obj .execute("INSERT INTO kv_list (key, value) VALUES (?1, ?2)",&[&key, &value]);
+        self.load_records();
+        if self.list.contains_key(key){
+            let update_result=self.db_obj .execute("UPDATE kv_list SET value=?1 WHERE key=?2",&[&value, &key]);
+            if update_result.is_ok(){
+                if update_result.unwrap()>0{
+                    self.list.insert(String::from(key),String::from(value));
+                    return true;
+                }
+            }
+        }else{
+            let insert_result=self.db_obj .execute("INSERT INTO kv_list (key, value) VALUES (?1, ?2)",&[&key, &value]);
+            if insert_result.is_ok(){
+                if insert_result.unwrap()>0{
+                    self.list.insert(String::from(key),String::from(value));
+                    return true;
+                }
+            }
+        }
+        false
+    }
+    pub fn get_value(&mut self,key:&str) -> Option<String> {
+        if self.list.contains_key(key){
+            let value_str=self.list.get(key);
+            Some(String::from(value_str.unwrap()))
+        }else{
+            None
+        }
+    }
+
+    pub fn delete(&mut self,key:&str) ->bool {
+        if self.db_open==false {
+            return false;
+        }
+        self.load_records();
+        let insert_result=self.db_obj .execute("DELETE FROM kv_list WHERE key = ?1",&[&key]);
         if insert_result.is_ok(){
             if insert_result.unwrap()>0{
+                self.list.remove(&key.to_string());
                 return true;
             }
         }
         false
     }
+
     pub fn flush(&mut self) -> bool{
         self.close();
         if std::fs::metadata(&self.db_path).is_ok(){
@@ -79,47 +114,8 @@ impl KeyValueDb {
         if self.db_open==true {
             self.db_obj=Connection::open_in_memory().unwrap();
             self.db_open=false;
-        }
-    }
-    pub fn delete(&mut self,key:&str) ->bool {
-        if self.db_open==false {
-            return false;
-        }
-        let insert_result=self.db_obj .execute("DELETE FROM kv_list WHERE key = ?1",&[&key]);
-        if insert_result.is_ok(){
-            if insert_result.unwrap()>0{
-                return true;
-            }
-        }
-        false
-    }
-    pub fn get_value(&mut self,key:&str) -> Option<String> {
-        if self.db_open==false {
-            return None;
-        }
-
-        let stmt = self.db_obj.prepare("SELECT value FROM kv_list WHERE key=:key");
-        let mut result_str=String::new();
-        let mut founded=false;
-        if stmt.is_ok(){
-            let mut stmt=stmt.unwrap();
-            stmt.query_map(
-                &[(":key", key.to_string().as_str())], 
-                |row| {
-                    let row_result:String=row.get(0).unwrap();
-                    Ok(row_result)
-                }
-            ).into_iter().for_each(|person| {
-                person.for_each(|s|{
-                    founded=true;
-                    result_str=s.unwrap();
-                });
-            });            
-        }
-        if founded==true{
-            Some(result_str)
-        }else{
-            None
+            self.list.clear();
+            self.record_loaded=false;
         }
     }
 }
@@ -127,16 +123,25 @@ impl KeyValueDb {
 #[test]
 fn full_test() {
     // cargo test  --lib full_test -- --nocapture
-    let mut kv_obj=KeyValueDb::new("db_name");
-    kv_obj.add("key1", "value");
-    kv_obj.flush();
-    //kv_obj.add("key2", "value");
-    //kv_obj.add("key3", "value");
-    //kv_obj.add("key4", "value");
-    //kv_obj.delete("key3");
-    //let rst=kv_obj.get_value("key3");
-    //dbg!(rst);
-    
-    
-    assert_eq!(true,true)
+    let since_the_epoch = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH);
+    if since_the_epoch.is_ok(){
+        let tmp_db_name=since_the_epoch.unwrap().as_micros().to_string();
+        let mut kv_obj=KeyValueDb::new(&tmp_db_name);
+        kv_obj.set_value("tmp_key", "tmp_value");
+        let mut record_founded=false;
+        let tmp_value=kv_obj.get_value("tmp_key");
+        if tmp_value.is_some(){   
+            if tmp_value.unwrap().eq("tmp_value"){
+                record_founded=true;
+            }
+        }
+        kv_obj.flush();
+        if record_founded==true{
+            assert_eq!(true,true)
+        }else{
+            assert_eq!(true,false)
+        }
+    }else{
+        assert_eq!(true,false)
+    }
 }
